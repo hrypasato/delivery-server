99b5968d065ee6de7c1bff172dd472188124f39a
hrypasato <hrypasato@gmail.com>
2022-11-29 16:03:37 -0500

    build[env]: install dotenv & setup vercel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express"); 
 const cors = require('cors');
+const dotenv = require('dotenv')
 
 const v1PedidosRouter = require('./pedidos/pedidoRouter');
 const v1ProductosRouter = require('./productos/productoRouter');
 
+dotenv.config()
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
