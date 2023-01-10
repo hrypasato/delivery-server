27a2642b9e694a6b7d4b1a2f92bc28400d6ac003
hrypasato <hrypasato@gmail.com>
2023-01-09 21:12:08 -0500

    feat[routes]: create routes index

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@ const express = require("express");
 const cors = require('cors');
 const dotenv = require('dotenv')
 
-const v1PedidosRouter = require('./pedidos/pedidoRouter');
-const v1ProductosRouter = require('./productos/productoRouter');
-const v1RepartidoresRouter = require('./repartidores/repartidorRouter');
-const v1TiendasRouter = require('./tiendas/tiendaRouter');
+const { rutas } = require('./routes');
+
 
 dotenv.config()
 
@@ -15,9 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/tiendas", v1TiendasRouter);
-app.use("/api/v1/pedidos", v1PedidosRouter);
-app.use("/api/v1/productos", v1ProductosRouter);
-app.use("/api/v1/repartidores", v1RepartidoresRouter);
+rutas.forEach( ruta => {
+    app.use(ruta.path, ruta.router);
+})
+
+
 
 module.exports = app;
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const ProductosRouterV1 = require('../productos/productoRouter');
+const RepartidoresRouterV1 = require('../repartidores/repartidorRouter');
+const TiendasRouterV1 = require('../tiendas/tiendaRouter');
+const PedidoRouterV1 = require('../pedidos/pedidoRouter');
+
+const rutas = [
+    { path:'/api/v1/pedidos', router: PedidoRouterV1 }, 
+    { path:"/api/v1/productos", router: ProductosRouterV1 },
+    { path:"/api/v1/tiendas", router: TiendasRouterV1 },
+    { path:"/api/v1/repartidores", router: RepartidoresRouterV1 },
+];
+
+module.exports = {
+    rutas,
+}
