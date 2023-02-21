8ee6ad0fa1f85e796a5497d1bdb575007a6cdbcd
hrypasato <hrypasato@gmail.com>
2023-02-21 10:13:00 -0500

    add swagger documentation

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require("express"); 
 const cors = require('cors');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
-const { rutas } = require('./router');
-
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 dotenv.config()
 
-const app = express();  
 
+const { rutas } = require('./router');
+
+const app = express();  
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +19,7 @@ rutas.forEach( ruta => {
     app.use(ruta.path, ruta.router);
 })
 
+app.use('/docs',swaggerUI.serve ,swaggerUI.setup(swaggerDocument));
 
 
 module.exports = app;
